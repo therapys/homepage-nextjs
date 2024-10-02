2f7e43776e4d45d94a9bf61948296aaa36c85df7
therapys <matumcbot2@gmail.com>
2024-10-02 12:25:56 +0300

    change: fixed apostrophes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,82 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
       <main className="block isolate">
-        <h1>hi i'm karl</h1>
-        
+        <h1>hi i&apos;m karl</h1>
+
         <h3>i write code, take photos, sometimes pentest.</h3>
 
         <p className="block mr-2">
-          i've made a start-up, <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="https://strikt.tech/">strikt.tech</a>.
+          i&apos;ve made a start-up,{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="https://strikt.tech/"
+          >
+            strikt.tech
+          </a>
+          .
         </p>
 
         <p>
-          i make and run <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="https://minecraft.net/" target="_blank">block game</a> servers. 
-          some of them are listed under <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="/projects/">projects</a>.
-        </p>
-        
-        <p>
-          you can check out my doings from <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="/gallery/">gallery</a> and <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="https://github.com/therapys">github</a>. 
+          i make and run{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="https://minecraft.net/"
+            target="_blank"
+          >
+            block game
+          </a>{" "}
+          servers. some of them are listed under{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="/projects/"
+          >
+            projects
+          </a>
+          .
         </p>
 
         <p>
-          every once in a while I happen to do something that I want to share with people in detail. 
-          that's what <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="/blog/">blog</a> is for.
+          you can check out my doings from{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="/gallery/"
+          >
+            gallery
+          </a>{" "}
+          and{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="https://github.com/therapys"
+          >
+            github
+          </a>
+          .
         </p>
 
         <p>
-          this site is heavily inspired by <a className="text-blue-500 cursor-pointer no-underline hover:underline" href="https://bearblog.dev/">bearblog</a>. i heavily recommend you check it out.
+          every once in a while I happen to do something that I want to share
+          with people in detail. that&apos;s what{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="/blog/"
+          >
+            blog
+          </a>{" "}
+          is for.
         </p>
 
-      </main> 
+        <p>
+          this site is heavily inspired by{" "}
+          <a
+            className="text-blue-500 cursor-pointer no-underline hover:underline"
+            href="https://bearblog.dev/"
+          >
+            bearblog
+          </a>
+          . i heavily recommend you check it out.
+        </p>
+      </main>
     </div>
   );
 }
