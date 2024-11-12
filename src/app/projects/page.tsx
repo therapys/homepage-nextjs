9ac936ea0dc559011f35c4f74d5997ad19fa6d67
therapys <matumcbot2@gmail.com>
2024-11-12 11:45:54 +0200

    feat: made projects have hrefs

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,40 @@
-
-
 export default function Projects() {
   return (
-    <>
-    </>
-  )
-}
+      <>
+          <h1>
+              projects
+          </h1>
 
+          <li>
+            <a
+                className="text-blue-500 cursor-pointer no-underline hover:underline"
+                href="https://strikt.tech"
+            > strikt.tech </a>
+          </li>
+
+          <li>
+              bosshub.net (discontinued.)
+          </li>
+
+          <li>
+              pvplabs.org (discontinued.)
+          </li>
+
+          <li>
+            <a
+                className="text-blue-500 cursor-pointer no-underline hover:underline"
+                href="https://mcrivals.com"
+            > 
+             mcrivals.com
+            </a>
+          </li>
+          <li>
+            <a
+                    className="text-blue-500 cursor-pointer no-underline hover:underline"
+                    href="https://volton.ee"
+                > 
+             volton.ee
+            </a>
+          </li>
+      </>);
+}
